25f11d64540c67b1963fc0fd839cea5e6f10e786
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-01-16 11:09:00 -0500

    pseudocode for javascript

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//Have current date and time displayed
+
+//create timeblocks for the planner using business hours
+
+//check time of day (past/present/future)
+
+//save to local storage
+
+//get from local storage
